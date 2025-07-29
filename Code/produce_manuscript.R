ba2262b0446958a8855ae470bb1a65e72b7574a4
Ben Goodair <ben.goodair@outlook.com>
2025-07-29 11:00:05 +0100

    numerous occasions

--- Code/produce_manuscript.R
+++ Code/produce_manuscript.R
@@ -40,5 +40,5 @@ figure_4 <- create_figure_4()
 #ggsave(plot=figure_1, filename="~/Library/CloudStorage/OneDrive-Nexus365/Documents/GitHub/Github_new/Care-Markets/Figures/figure_1_revised.jpeg", width=9.5, height=7, dpi=600)
 #ggsave(plot=figure_2, filename="~/Library/CloudStorage/OneDrive-Nexus365/Documents/GitHub/Github_new/Care-Markets/Figures/figure_2_revised.jpeg", width=11, height=6, dpi=600)
 #ggsave(plot=figure_3, filename="~/Library/CloudStorage/OneDrive-Nexus365/Documents/GitHub/Github_new/Care-Markets/Figures/figure_3_revised.jpeg", width=12, height=11, dpi=600)
-# ggsave(plot=figure_4, filename="~/Library/CloudStorage/OneDrive-Nexus365/Documents/GitHub/Github_new/Care-Markets/Figures/figure_4_revised.jpeg", width=7, height=15, dpi=600)
+#ggsave(plot=figure_4, filename="~/Library/CloudStorage/OneDrive-Nexus365/Documents/GitHub/Github_new/Care-Markets/Figures/figure_4_revised.jpeg", width=7, height=16, dpi=600)
 
