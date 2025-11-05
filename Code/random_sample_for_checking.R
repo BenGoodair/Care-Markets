b6da5ba4b098e56a85e72cad244b3a1d79cdb4ad
Ben Goodair <ben.goodair@outlook.com>
2025-11-05 15:48:13 +0000

    fist in the sky

---
+++ Code/random_sample_for_checking.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+
+final_data <- read_csv("Downloads/final_data.csv")
+
+random_sample <- final_data %>%
+  filter(Sector.x != "Local Authority", Sector.x != "Voluntary") %>%
+  sample_n(30)
+
+
+write.csv(random_sample, "~/Library/CloudStorage/OneDrive-Nexus365/Documents/GitHub/Github_new/Care-Markets/Final Data/manual_check_sample.csv")
+
+
+final_data <- read_csv("Downloads/final_data.csv")
+
+checks <- read.csv( "Library/CloudStorage/OneDrive-Nexus365/incorrect_corporate_companies.csv")
+
+
+yep <- final_data%>%dplyr::select(Sector_merge, Company.name, URN) %>%
+  dplyr::full_join(., checks%>% dplyr::select(Company.name, Registered.number)%>%dplyr::mutate(charity="yes"))%>%
+  dplyr::filter(charity=="yes")
+
+wrong <- yep %>% dplyr::filter(Sector_merge=="Corporate owned")
+
+write.csv(wrong, "~/Library/CloudStorage/OneDrive-Nexus365/Documents/GitHub/Github_new/Care-Markets/Final Data/all_missing_info.csv")
